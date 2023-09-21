e3f9901bfd1e56f7ebf417645c6b337323b23bfa
manuvats <manuvats1990@gmail.com>
2023-09-21 18:25:15 +0100

    Requirements added

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Stock_Price_Volatility_Prediction"
+AUTHOR_USER_NAME = "manuvats"
+SRC_REPO = "StockVolatility"
+AUTHOR_EMAIL = "manuvats1990@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="Stock Price Volatility Prediction app using Tweets about stocks, Stock News Headlines and Google Trends",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
